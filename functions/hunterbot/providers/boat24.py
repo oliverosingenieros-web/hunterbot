97fdd3b3a529150c6a9f77345030ad5699788a98
oliverosingenieros-web <oliverosingenieros@gmail.com>
2026-08-25 10:37:43 +0200

    sync: functions/hunterbot/providers/boat24.py

--- functions/hunterbot/providers/boat24.py
+++ functions/hunterbot/providers/boat24.py
@@ -41,19 +41,19 @@ class Boat24Provider(BaseProvider):
                 return []
 
             parser = HTMLParser(resp.text)
-            cards = (
+            cards = self.css_from_config(parser, "card") or (
                 parser.css(".blist__item")
                 or parser.css(".row.blist")
                 or parser.css("article")
             )
 
             for card in cards:
-                link_el = card.css_first("a[href*='/detalle/']") or card.css_first("a")
+                link_el = self.css_first_from_config(card, "link") or card.css_first("a[href*='/detalle/']") or card.css_first("a")
                 if not link_el:
                     continue
 
                 href = link_el.attributes.get("href") or ""
-                title_el = (
+                title_el = self.css_first_from_config(card, "title") or (
                     card.css_first(".blist__title")
                     or card.css_first("h3")
                     or card.css_first("h2")
@@ -62,7 +62,7 @@ class Boat24Provider(BaseProvider):
                     title_el.text(strip=True) if title_el else link_el.text(strip=True)
                 )
 
-                price_el = card.css_first(".blist__price") or card.css_first(
+                price_el = self.css_first_from_config(card, "price") or card.css_first(".blist__price") or card.css_first(
                     "[class*='price']"
                 )
                 price_text = price_el.text(strip=True) if price_el else ""
